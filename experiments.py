1782633fdf2235f6e1044b97e9fce276091dbf7f
Qi Zhang <52573258+TracReggie@users.noreply.github.com>
2023-11-18 15:55:54 +0800

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -5,18 +5,17 @@ from torch_geometric.loader import NeighborLoader, DataLoader
 from training_tools import Logger, set_seed
 from graph_tools import kmeans_divide, SC_divide, metis_divide, get_subgraph, GPPool
 from model import GCN, Net_APPNP, SAGE
-from prepare_data import return_dataset
 
 
 
-def get_train_loader(divide_list, data, gppool, y_setting, batch_size):
+def get_train_loader(divide_list, num_classes, data, gppool, y_setting, batch_size):
     train_loader = []
     parts = torch.tensor(list(range(len(divide_list))))
     loader = DataLoader(parts, batch_size=batch_size, shuffle=True)
 
     if gppool:
         for unpooled_parts in loader:
-            pooled_g = GPPool(data, divide_list, unpooled_parts, y_setting)
+            pooled_g = GPPool(data, num_classes, divide_list, unpooled_parts, y_setting)
             train_loader.append(pooled_g)
     else:
         for part in divide_list:
@@ -68,9 +67,10 @@ def test(model, data, backbone, test_loader, device):
 
 
 
-def run_experiments(dataset, backbone, divide_method, num_subgraphs, y_setting, gppool, lr, weight_decay, epochs, eval_steps, runs, cuda):
+def run_experiments(data, num_classes, backbone, divide_method, num_subgraphs, y_setting, gppool,
+                    lr, weight_decay, epochs, eval_steps, runs, cuda):
+
     set_seed()
-    data = return_dataset(f'{dataset}')
     device = torch.device(f'cuda:{cuda}')
 
     masks = [data.train_mask, data.val_mask, data.test_mask]
@@ -84,25 +84,27 @@ def run_experiments(dataset, backbone, divide_method, num_subgraphs, y_setting, 
         divide_list = SC_divide(data.x, num_subgraphs)
 
     if backbone == "GCN":
-        model = GCN(data.num_features, 512, data.num_classes, 2, 0.2).to(device)
+        model = GCN(data.num_features, 512, num_classes, 2, 0.2).to(device)
     elif backbone == "APPNP":
-        model = Net_APPNP(data.num_features, 512, data.num_classes, 2, 0.2).to(device)
+        model = Net_APPNP(data.num_features, 512, num_classes, 2, 0.2).to(device)
     elif backbone == "SAGE":
-        model = SAGE(data.num_features, 512, data.num_classes, 3, 0.2).to(device)
+        model = SAGE(data.num_features, 512, num_classes, 3, 0.2).to(device)
 
-    train_loader = get_train_loader(divide_list, data, gppool, y_setting, int(num_subgraphs / 4))
-    test_loader = NeighborLoader(data, num_neighbors=[-1], shuffle=False, batch_size=15000, num_workers=16, persistent_workers=True)
+    train_loader = get_train_loader(divide_list, num_classes, data, gppool, y_setting, int(num_subgraphs / 4))
+    test_loader = NeighborLoader(data, num_neighbors=[-1], shuffle=False, batch_size=15000,
+                                 num_workers=16, persistent_workers=True)
 
     logger = Logger(runs)
     for run in range(runs):
         print('============================================')
         model.reset_parameters()
+
         optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 
         for epoch in range(1, 1 + epochs):
             loss = train(model, train_loader, optimizer, device)
             print(f'Run: {run + 1:02d}, Epoch: {epoch:02d}, Loss: {loss:.4f}')
-            train_loader = get_train_loader(divide_list, data, gppool, y_setting, int(num_subgraphs / 4))
+            train_loader = get_train_loader(divide_list, num_classes, data, gppool, y_setting, int(num_subgraphs / 4))
 
             if epoch > 19 and epoch % eval_steps == 0:
                 train_acc, val_acc, test_acc = test(model, data, backbone, test_loader, device)
@@ -111,4 +113,6 @@ def run_experiments(dataset, backbone, divide_method, num_subgraphs, y_setting, 
                 logger.add_result(run, result)
 
         logger.print_statistics(run)
-    logger.print_statistics()
+    acc = logger.print_statistics()
+
+    return acc
